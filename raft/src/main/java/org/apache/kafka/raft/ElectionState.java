0dc2106073cc2bfdc3c6bdf4e694be65b0ce9038
automation <automation@mygamma.io>
2021-04-05 17:36:48 +0530

    commitMessage : MINOR: Add raft resigned state metric name

--- raft/src/main/java/org/apache/kafka/raft/ElectionState.java
+++ raft/src/main/java/org/apache/kafka/raft/ElectionState.java
@@ -100,6 +100,7 @@ public class ElectionState {
         return "Election(epoch=" + epoch +
                 ", leaderIdOpt=" + leaderIdOpt +
                 ", votedIdOpt=" + votedIdOpt +
+                ", voters=" + voters +
                 ')';
     }
 

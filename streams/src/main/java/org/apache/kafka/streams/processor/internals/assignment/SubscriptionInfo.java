507d3df9e4a421d9e0236e0566717849c3c08873
automation <automation@mygamma.io>
2021-04-06 12:39:15 +0530

    commitMessage : Feature/refactor subscription info

--- streams/src/main/java/org/apache/kafka/streams/processor/internals/assignment/SubscriptionInfo.java
+++ streams/src/main/java/org/apache/kafka/streams/processor/internals/assignment/SubscriptionInfo.java
@@ -16,302 +16,157 @@
  */
 package org.apache.kafka.streams.processor.internals.assignment;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-
-import org.apache.kafka.common.Uuid;
-import org.apache.kafka.common.protocol.ByteBufferAccessor;
-import org.apache.kafka.common.protocol.MessageUtil;
 import org.apache.kafka.streams.errors.TaskAssignmentException;
-import org.apache.kafka.streams.internals.generated.SubscriptionInfoData;
-import org.apache.kafka.streams.internals.generated.SubscriptionInfoData.PartitionToOffsetSum;
-import org.apache.kafka.streams.internals.generated.SubscriptionInfoData.TaskOffsetSum;
 import org.apache.kafka.streams.processor.TaskId;
-import org.apache.kafka.streams.processor.internals.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
-import java.util.Collections;
+import java.nio.charset.Charset;
 import java.util.Set;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
-import static org.apache.kafka.streams.processor.internals.assignment.StreamsAssignmentProtocolVersions.LATEST_SUPPORTED_VERSION;
+public abstract class SubscriptionInfo {
 
-public class SubscriptionInfo {
-    private static final Logger LOG = LoggerFactory.getLogger(SubscriptionInfo.class);
+    public static final int LATEST_SUPPORTED_VERSION = 2;
+    private static final Logger log = LoggerFactory.getLogger(SubscriptionInfo.class);
+    protected UUID processId;
+    protected Set<TaskId> prevTasks;
+    protected Set<TaskId> standbyTasks;
+    protected String userEndPoint;
 
-    static final int UNKNOWN = -1;
-    static final int MIN_VERSION_OFFSET_SUM_SUBSCRIPTION = 7;
-    public static final long UNKNOWN_OFFSET_SUM = -3L;
 
-    private final SubscriptionInfoData data;
-    private Set<TaskId> prevTasksCache = null;
-    private Set<TaskId> standbyTasksCache = null;
-    private Map<TaskId, Long> taskOffsetSumsCache = null;
+    public SubscriptionInfo() {
+    }
 
-    static {
-        // Just statically check to make sure that the generated code always stays in sync with the overall protocol
-        final int subscriptionInfoLatestVersion = SubscriptionInfoData.SCHEMAS.length - 1;
-        if (subscriptionInfoLatestVersion != LATEST_SUPPORTED_VERSION) {
-            throw new IllegalArgumentException(
-                "streams/src/main/resources/common/message/SubscriptionInfo.json needs to be updated to match the " +
-                    "latest assignment protocol version. SubscriptionInfo only supports up to  ["
-                    + subscriptionInfoLatestVersion + "] but needs to support up to [" + LATEST_SUPPORTED_VERSION + "].");
+    public SubscriptionInfo(final UUID processId,
+                            final Set<TaskId> prevTasks,
+                            final Set<TaskId> standbyTasks,
+                            final String userEndPoint) {
+        this.processId = processId;
+        this.prevTasks = prevTasks;
+        this.standbyTasks = standbyTasks;
+        this.userEndPoint = userEndPoint;
+    }
+
+    static private SubscriptionInfo createSubscriptionInfo(final int version) {
+        switch (version) {
+            case 1:
+                return new SubscriptionInfoV1();
+            case 2:
+                return new SubscriptionInfoV2();
+            default:
+                throw new IllegalStateException("Unknown metadata version: " + version
+                        + "; latest supported version: " + LATEST_SUPPORTED_VERSION);
+
         }
     }
 
-    private static void validateVersions(final int version, final int latestSupportedVersion) {
-        if (latestSupportedVersion == UNKNOWN && (version < 1 || version > 2)) {
-            throw new IllegalArgumentException(
-                "Only versions 1 and 2 are expected to use an UNKNOWN (-1) latest supported version. " +
-                    "Got " + version + "."
-            );
-        } else if (latestSupportedVersion != UNKNOWN && (version < 1 || version > latestSupportedVersion)) {
-            throw new IllegalArgumentException(
-                "version must be between 1 and " + latestSupportedVersion + "; was: " + version
-            );
+    static public SubscriptionInfo createSubsriptionInfo(final UUID processId,
+                                                         final Set<TaskId> prevTasks,
+                                                         final Set<TaskId> standbyTasks,
+                                                         final String userEndPoint) {
+        return new SubscriptionInfoV2(processId, prevTasks, standbyTasks, userEndPoint);
+    }
+
+    static public SubscriptionInfo createSubsriptionInfo(final int version,
+                                                         final UUID processId,
+                                                         final Set<TaskId> prevTasks,
+                                                         final Set<TaskId> standbyTasks,
+                                                         final String userEndPoint) {
+        switch (version) {
+            case 1:
+                return new SubscriptionInfoV1(processId, prevTasks, standbyTasks, userEndPoint);
+            case 2:
+                return new SubscriptionInfoV2(processId, prevTasks, standbyTasks, userEndPoint);
+            default:
+                throw new IllegalStateException("Unknown metadata version: " + version
+                        + "; latest supported version: " + LATEST_SUPPORTED_VERSION);
+
         }
-    }
-
-    public SubscriptionInfo(final int version,
-                            final int latestSupportedVersion,
-                            final UUID processId,
-                            final String userEndPoint,
-                            final Map<TaskId, Long> taskOffsetSums,
-                            final byte uniqueField,
-                            final int errorCode) {
-        validateVersions(version, latestSupportedVersion);
-        final SubscriptionInfoData data = new SubscriptionInfoData();
-        data.setVersion(version);
-        data.setProcessId(new Uuid(processId.getMostSignificantBits(),
-                processId.getLeastSignificantBits()));
-
-        if (version >= 2) {
-            data.setUserEndPoint(userEndPoint == null
-                                     ? new byte[0]
-                                     : userEndPoint.getBytes(StandardCharsets.UTF_8));
-        }
-        if (version >= 3) {
-            data.setLatestSupportedVersion(latestSupportedVersion);
-        }
-        if (version >= 8) {
-            data.setUniqueField(uniqueField);
-        }
-        if (version >= 9) {
-            data.setErrorCode(errorCode);
-        }
-
-        this.data = data;
-
-        if (version >= MIN_VERSION_OFFSET_SUM_SUBSCRIPTION) {
-            setTaskOffsetSumDataFromTaskOffsetSumMap(taskOffsetSums);
-        } else {
-            setPrevAndStandbySetsFromParsedTaskOffsetSumMap(taskOffsetSums);
-        }
-    }
-
-    private SubscriptionInfo(final SubscriptionInfoData subscriptionInfoData) {
-        validateVersions(subscriptionInfoData.version(), subscriptionInfoData.latestSupportedVersion());
-        this.data = subscriptionInfoData;
-    }
-
-    public int errorCode() {
-        return data.errorCode();
-    }
-
-    private void setTaskOffsetSumDataFromTaskOffsetSumMap(final Map<TaskId, Long> taskOffsetSums) {
-        final Map<Integer, List<SubscriptionInfoData.PartitionToOffsetSum>> topicGroupIdToPartitionOffsetSum = new HashMap<>();
-        for (final Map.Entry<TaskId, Long> taskEntry : taskOffsetSums.entrySet()) {
-            final TaskId task = taskEntry.getKey();
-            topicGroupIdToPartitionOffsetSum.computeIfAbsent(task.topicGroupId, t -> new ArrayList<>()).add(
-                new SubscriptionInfoData.PartitionToOffsetSum()
-                    .setPartition(task.partition)
-                    .setOffsetSum(taskEntry.getValue()));
-        }
-
-        data.setTaskOffsetSums(topicGroupIdToPartitionOffsetSum.entrySet().stream().map(t -> {
-            final SubscriptionInfoData.TaskOffsetSum taskOffsetSum = new SubscriptionInfoData.TaskOffsetSum();
-            taskOffsetSum.setTopicGroupId(t.getKey());
-            taskOffsetSum.setPartitionToOffsetSum(t.getValue());
-            return taskOffsetSum;
-        }).collect(Collectors.toList()));
-    }
-
-    private void setPrevAndStandbySetsFromParsedTaskOffsetSumMap(final Map<TaskId, Long> taskOffsetSums) {
-        final Set<TaskId> prevTasks = new HashSet<>();
-        final Set<TaskId> standbyTasks = new HashSet<>();
-
-        for (final Map.Entry<TaskId, Long> taskOffsetSum : taskOffsetSums.entrySet()) {
-            if (taskOffsetSum.getValue() == Task.LATEST_OFFSET) {
-                prevTasks.add(taskOffsetSum.getKey());
-            } else {
-                standbyTasks.add(taskOffsetSum.getKey());
-            }
-        }
-
-        data.setPrevTasks(prevTasks.stream().map(t -> {
-            final SubscriptionInfoData.TaskId taskId = new SubscriptionInfoData.TaskId();
-            taskId.setTopicGroupId(t.topicGroupId);
-            taskId.setPartition(t.partition);
-            return taskId;
-        }).collect(Collectors.toList()));
-        data.setStandbyTasks(standbyTasks.stream().map(t -> {
-            final SubscriptionInfoData.TaskId taskId = new SubscriptionInfoData.TaskId();
-            taskId.setTopicGroupId(t.topicGroupId);
-            taskId.setPartition(t.partition);
-            return taskId;
-        }).collect(Collectors.toList()));
-    }
-
-    public int version() {
-        return data.version();
-    }
-
-    public int latestSupportedVersion() {
-        return data.latestSupportedVersion();
-    }
-
-    public UUID processId() {
-        return new UUID(data.processId().getMostSignificantBits(), data.processId().getLeastSignificantBits());
-    }
-
-    public Set<TaskId> prevTasks() {
-        if (prevTasksCache == null) {
-            if (data.version() >= MIN_VERSION_OFFSET_SUM_SUBSCRIPTION) {
-                prevTasksCache = getActiveTasksFromTaskOffsetSumMap(taskOffsetSums());
-            } else {
-                prevTasksCache = Collections.unmodifiableSet(
-                    data.prevTasks()
-                        .stream()
-                        .map(t -> new TaskId(t.topicGroupId(), t.partition()))
-                        .collect(Collectors.toSet())
-                );
-            }
-        }
-        return prevTasksCache;
-    }
-
-    public Set<TaskId> standbyTasks() {
-        if (standbyTasksCache == null) {
-            if (data.version() >= MIN_VERSION_OFFSET_SUM_SUBSCRIPTION) {
-                standbyTasksCache = getStandbyTasksFromTaskOffsetSumMap(taskOffsetSums());
-            } else {
-                standbyTasksCache = Collections.unmodifiableSet(
-                    data.standbyTasks()
-                        .stream()
-                        .map(t -> new TaskId(t.topicGroupId(), t.partition()))
-                        .collect(Collectors.toSet())
-                );
-            }
-        }
-        return standbyTasksCache;
-    }
-
-    public Map<TaskId, Long> taskOffsetSums() {
-        if (taskOffsetSumsCache == null) {
-            taskOffsetSumsCache = new HashMap<>();
-            if (data.version() >= MIN_VERSION_OFFSET_SUM_SUBSCRIPTION) {
-                for (final TaskOffsetSum taskOffsetSum : data.taskOffsetSums()) {
-                    for (final PartitionToOffsetSum partitionOffsetSum : taskOffsetSum.partitionToOffsetSum()) {
-                        taskOffsetSumsCache.put(
-                            new TaskId(taskOffsetSum.topicGroupId(),
-                                       partitionOffsetSum.partition()),
-                            partitionOffsetSum.offsetSum()
-                        );
-                    }
-                }
-            } else {
-                prevTasks().forEach(taskId -> taskOffsetSumsCache.put(taskId, Task.LATEST_OFFSET));
-                standbyTasks().forEach(taskId -> taskOffsetSumsCache.put(taskId, UNKNOWN_OFFSET_SUM));
-            }
-        }
-        return taskOffsetSumsCache;
-    }
-
-    public String userEndPoint() {
-        return data.userEndPoint() == null || data.userEndPoint().length == 0
-            ? null
-            : new String(data.userEndPoint(), StandardCharsets.UTF_8);
-    }
-
-    public static Set<TaskId> getActiveTasksFromTaskOffsetSumMap(final Map<TaskId, Long> taskOffsetSums) {
-        return taskOffsetSumMapToTaskSet(taskOffsetSums, true);
-    }
-
-    public static Set<TaskId> getStandbyTasksFromTaskOffsetSumMap(final Map<TaskId, Long> taskOffsetSums) {
-        return taskOffsetSumMapToTaskSet(taskOffsetSums, false);
-    }
-
-    private static Set<TaskId> taskOffsetSumMapToTaskSet(final Map<TaskId, Long> taskOffsetSums,
-                                                         final boolean getActiveTasks) {
-        return taskOffsetSums.entrySet().stream()
-                   .filter(t -> getActiveTasks == (t.getValue() == Task.LATEST_OFFSET))
-                   .map(Map.Entry::getKey)
-                   .collect(Collectors.toSet());
-    }
-
-    /**
-     * @throws TaskAssignmentException if method fails to encode the data
-     */
-    public ByteBuffer encode() {
-        if (data.version() > LATEST_SUPPORTED_VERSION) {
-            throw new IllegalStateException(
-                "Should never try to encode a SubscriptionInfo with version [" +
-                    data.version() + "] > LATEST_SUPPORTED_VERSION [" + LATEST_SUPPORTED_VERSION + "]"
-            );
-        } else return MessageUtil.toByteBuffer(data, (short) data.version());
     }
 
     /**
      * @throws TaskAssignmentException if method fails to decode the data
      */
     public static SubscriptionInfo decode(final ByteBuffer data) {
+        // ensure we are at the beginning of the ByteBuffer
         data.rewind();
-        final int version = data.getInt();
 
-        if (version > LATEST_SUPPORTED_VERSION) {
-            // in this special case, we only rely on the version and latest version,
-            //
-            final int latestSupportedVersion = data.getInt();
-            final SubscriptionInfoData subscriptionInfoData = new SubscriptionInfoData();
-            subscriptionInfoData.setVersion(version);
-            subscriptionInfoData.setLatestSupportedVersion(latestSupportedVersion);
-            LOG.info("Unable to decode subscription data: used version: {}; latest supported version: {}",
-                version,
-                latestSupportedVersion
-            );
-            return new SubscriptionInfo(subscriptionInfoData);
+        // decode used version
+        final int usedVersion = data.getInt();
+        final SubscriptionInfo subscriptionInfo = createSubscriptionInfo(usedVersion);
+        subscriptionInfo.doDecode(data);
+
+        return subscriptionInfo;
+    }
+
+    abstract public int version();
+
+    public UUID processId() {
+        return processId;
+    }
+
+    public Set<TaskId> prevTasks() {
+        return prevTasks;
+    }
+
+    public Set<TaskId> standbyTasks() {
+        return standbyTasks;
+    }
+
+    public String userEndPoint() {
+        return userEndPoint;
+    }
+
+    abstract protected ByteBuffer doEncode(byte[] endPointBytes);
+
+    /**
+     * @throws TaskAssignmentException if method fails to encode the data
+     */
+    public ByteBuffer encode() {
+        final ByteBuffer buf = doEncode(prepareUserEndPoint());
+        buf.rewind();
+        return buf;
+    }
+
+    abstract protected int getByteLength(final byte[] endPointBytes);
+
+    private byte[] prepareUserEndPoint() {
+        if (userEndPoint == null) {
+            return new byte[0];
         } else {
-            data.rewind();
-            final ByteBufferAccessor accessor = new ByteBufferAccessor(data);
-            final SubscriptionInfoData subscriptionInfoData = new SubscriptionInfoData(accessor, (short) version);
-            return new SubscriptionInfo(subscriptionInfoData);
+            return userEndPoint.getBytes(Charset.forName("UTF-8"));
         }
     }
 
+    abstract protected void encodeData(final ByteBuffer buf,
+                                       final byte[] endPointBytes);
+
+    abstract protected void doDecode(ByteBuffer data);
+
+
     @Override
     public int hashCode() {
-        return data.hashCode();
+        final int hashCode = version() ^ processId.hashCode() ^ prevTasks.hashCode() ^ standbyTasks.hashCode();
+        if (userEndPoint == null) {
+            return hashCode;
+        }
+        return hashCode ^ userEndPoint.hashCode();
     }
 
     @Override
     public boolean equals(final Object o) {
         if (o instanceof SubscriptionInfo) {
             final SubscriptionInfo other = (SubscriptionInfo) o;
-            return data.equals(other.data);
+            return this.version() == other.version() &&
+                    this.processId.equals(other.processId) &&
+                    this.prevTasks.equals(other.prevTasks) &&
+                    this.standbyTasks.equals(other.standbyTasks) &&
+                    this.userEndPoint != null ? this.userEndPoint.equals(other.userEndPoint) : other.userEndPoint == null;
         } else {
             return false;
         }
     }
 
-    @Override
-    public String toString() {
-        return data.toString();
-    }
 }
